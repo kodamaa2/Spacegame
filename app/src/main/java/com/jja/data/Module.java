0d5bc9a6ef9a30a9c820c8b8b1ca01f628134c80
Julius <juli.braun@gmail.com>
2016-07-25 15:25:04 +0200

    Geschwindigkeitsmodul hinzugefügt

--- app/src/main/java/com/jja/data/Module.java
+++ app/src/main/java/com/jja/data/Module.java
@@ -5,11 +5,28 @@ package com.jja.data;
  */
 public class Module {
     private int mid, damage, damagetype;
+    private double speedBoost;
+
     public Module(int id){
         this.mid = id;
     }
 
     public int getDamage() {
         return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setDamagetype(int damagetype) {
+        this.damagetype = damagetype;
+    }
+    public double getSpeedBoost() {
+        return speedBoost;
+    }
+
+    public void setSpeedBoost(double speedBoost) {
+        this.speedBoost = speedBoost;
     }
 }
---
+++ app/src/main/java/com/jja/data/SpeedModule.java
@@ -0,0 +1,12 @@
+package com.jja.data;
+
+/**
+ * Created by Julius on 25.07.2016.
+ */
+public class SpeedModule extends Module{
+
+    public SpeedModule(int id, double spdboost){
+        super(id);
+        this.setSpeedBoost(spdboost);
+    }
+}
